cc11dbf81b45778905d55e23e1e03ff63a4dfb6d
himangsu roy <himur98@gmail.com>
2023-11-30 16:25:21 +0600

    added academic department controller

---
+++ src/app/modules/academicDepartment/academicDepartment.controller.ts
@@ -0,0 +1,69 @@
+import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
+import { AcademicDepartmentService } from './academicDepartment.service';
+
+const createAcademicDepartment = catchAsync(async (req, res) => {
+  const { academicDepartment } = req.body;
+  const result =
+    await AcademicDepartmentService.createAcademicDepartmentIntoDB(
+      academicDepartment,
+    );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Department created successfully',
+    data: result,
+  });
+});
+
+const getAllAcademicDepartments = catchAsync(async (req, res) => {
+  const result =
+    await AcademicDepartmentService.getAllAcademicDepartmentFromDB();
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Departments fetched successfully',
+    data: result,
+  });
+});
+
+const getSingleAcademicDepartment = catchAsync(async (req, res) => {
+  const { departmentId } = req.params;
+  const result =
+    await AcademicDepartmentService.getSingleAcademicDepartmentFromDB(
+      departmentId,
+    );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Department fetched successfully',
+    data: result,
+  });
+});
+
+const updateAcademicDepartment = catchAsync(async (req, res) => {
+  const { departmentId } = req.params;
+  const { academicDepartment } = req.body;
+  const result = await AcademicDepartmentService.updateAcademicDepartmentIntoDB(
+    departmentId,
+    academicDepartment,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic Department updated successfully',
+    data: result,
+  });
+});
+
+export const AcademicDepartmentController = {
+  createAcademicDepartment,
+  getAllAcademicDepartments,
+  getSingleAcademicDepartment,
+  updateAcademicDepartment,
+};
--- src/app/modules/academicFaculty/academicFaculty.validation.ts
+++ src/app/modules/academicFaculty/academicFaculty.validation.ts
@@ -1,21 +1,25 @@
 import { z } from 'zod';
 
 const createAcademicFacultyValidationSchema = z.object({
-  name: z
-    .string({
-      invalid_type_error: 'Academic faculty must be string',
-      required_error: 'Name is required',
-    })
-    .min(2, { message: 'Name must be at least 2 characters long' }),
+  body: z.object({
+    name: z
+      .string({
+        invalid_type_error: 'Academic faculty must be string',
+        required_error: 'Name is required',
+      })
+      .min(2, { message: 'Name must be at least 2 characters long' }),
+  }),
 });
 
 const updateAcademicFacultyValidationSchema = z.object({
-  name: z
-    .string({
-      invalid_type_error: 'Academic faculty must be string',
-      required_error: 'Name is required',
-    })
-    .min(2, { message: 'Name must be at least 2 characters long' }),
+  body: z.object({
+    name: z
+      .string({
+        invalid_type_error: 'Academic faculty must be string',
+        required_error: 'Name is required',
+      })
+      .min(2, { message: 'Name must be at least 2 characters long' }),
+  }),
 });
 
 export const AcademicFacultyValidation = {
