e9f367125440518bcb4e0e6f41ce7f81fff38a2a
himangsu roy <himur98@gmail.com>
2023-12-24 19:37:34 +0600

    added enrolledcourse validation

---
+++ src/app/modules/EnrolledCourse/enrolledCourse.service.ts
@@ -0,0 +1,12 @@
+import { TEnrolledCourse } from "./enrolledCourse.interface";
+
+const createEnrolledCourseIntoDB = async (userId: string, payload: TEnrolledCourse) => { 
+    
+};
+
+const updateEnrolldedCourseMarksIntoDB = async () => { };
+
+export const EnrolledCourseService = {
+  createEnrolledCourseIntoDB,
+  updateEnrolldedCourseMarksIntoDB,
+};
